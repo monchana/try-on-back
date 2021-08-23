c1e0b3eeaa3fe12b1409924899538d2bbeae06de
hsna <hsna@intellisys.co.kr>
2021-08-23 13:30:59 +0900

    changed line

--- cafe24/tryon/services/try_on_back_modules/tryongenerator/utils/html/layout_line.py
+++ cafe24/tryon/services/try_on_back_modules/tryongenerator/utils/html/layout_line.py
@@ -1,26 +1,30 @@
 
 def line(img_urls):
     front = '''
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <style>
-            .layout-line {
-            width: 100%;
-            }
-            .layout-line > * {
+        .layout-line {
+            max-width: 50%;
+        }
+        .layout-line > * {
             display: block;
+            justify-content: center;
+            align-self: center;
+
             margin: 0 auto;
-            margin-bottom: 0.5rem;
-            }
+            margin-bottom: 1.0rem;
+        }
         </style>
-        </head>
-        <body>
-        <div class="layout-line">
+    </head>
+    <body>
+        <div class="col-xs-1" align="center">
+            <div class="layout-line">
     '''
     
     middle = ''
@@ -30,6 +34,7 @@ def line(img_urls):
         middle += img_src
     
     end = '''
+        </div>
         </div>
     </body>
     </html>
